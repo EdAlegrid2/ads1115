c4d1aeb90c120818b8afd21ac34713de1b56953d
EdAlegrid2 <121866690+EdAlegrid2@users.noreply.github.com>
2023-01-26 16:00:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const r = require('array-gpio');
 
 let i2c = r.startI2C(1);    // using SDA1 and SCL1 pins (pin 3 & 5)
+//let i2c = r.startI2C(0);    // using SDA0 and SCL0 pins (pin 27 & 28)
 
 /* led conversion indicator (optional) */
 //let led = r.out(33); 
